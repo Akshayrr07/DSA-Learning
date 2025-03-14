da4090dea36f714c95fa9949231ab92332e4cdc9
Akshayrr07 <akshayrr04@gmail.com>
2025-03-14 17:17:33 +0530

    17.17

--- DAY 3/Double_linked/Pairly_Swap.py
+++ DAY 3/Double_linked/Pairly_Swap.py
@@ -1,47 +1,68 @@
 class Node:
     def __init__(self):
+        # Initialize a node with data and a pointer to the next node
         self.data = None
         self.next = None
 
 class LinkedList:
     def __init__(self):
+        # Initialize the linked list with a head pointer
         self.head = None
     
     def insert(self, data):
+        # Create a new node with the given data
         new_node = Node()
         new_node.data = data
         if self.head is None:
+            # If the list is empty, set the new node as the head
             self.head = new_node
         else:
+            # Traverse to the end of the list and append the new node
             temp = self.head
             while temp.next is not None:
                 temp = temp.next
             temp.next = new_node
     
     def display(self):
+        # Display the elements of the linked list
         if self.head is None:
+            # If the list is empty, print a message
             print("list is empty")
         else:
+            # Traverse the list and print each node's data
             temp = self.head
             while temp:
                 print(temp.data, end=" ")
-                temp =temp.next
+                temp = temp.next
     
     def swap(self):
-            temp = self.head
-            while temp and temp.next:
-                temp.data, temp.next.data = temp.next.data, temp.data
-                temp = temp.next.next
+        # Swap data of adjacent nodes in the linked list
+        temp = self.head
+        while temp and temp.next:
+            # Swap the data of the current node and the next node
+            temp.data, temp.next.data = temp.next.data, temp.data
+            # Move to the next pair of nodes
+            temp = temp.next.next
 
+# Create a linked list instance
 list = LinkedList()
+
+# Take input values from the user
 values = input().split()
 for val in values:
     val = int(val)
     if val < 0:
+        # Stop inserting values if a negative number is encountered
         break
     list.insert(val)
+
+# Display the original list
 print("The original List:\n")
 list.display()
+
+# Perform the pairwise swap of nodes
 list.swap()
+
+# Display the swapped list
 print("\nThe swapped List:\n")
 list.display()
--- DAY 3/Double_linked/merging_two_list.py
+++ DAY 3/Double_linked/merging_two_list.py
@@ -1,60 +1,111 @@
 class Node:
     def __init__(self, data):
+        # Initialize a node with data and a pointer to the next node
         self.data = data
         self.next = None
     
 class LinkedList:
     def __init__(self):
+        # Initialize an empty linked list with no head
         self.head = None
     
     def insert(self, data):
+        # Insert a new node at the beginning of the linked list
         newNode = Node(data)
         if self.head is None:
+            # If the list is empty, set the new node as the head
             self.head = newNode
         else:
+            # Otherwise, link the new node to the current head and update the head
             newNode.next = self.head
             self.head = newNode
     
-    def display(self):
+    def append(self, data):
+        # Append a new node at the end of the linked list
+        newNode = Node(data)
         if self.head is None:
-            print("List is empty")
+            # If the list is empty, set the new node as the head
+            self.head = newNode
         else:
+            # Traverse to the end of the list and add the new node
             temp = self.head
-            while temp:
-                print(temp.data, end=" ")
+            while temp.next:
                 temp = temp.next
+            temp.next = newNode
+
+    def display(self):
+        # Display the elements of the linked list
+        if self.head is None:
+            # If the list is empty, print a message
+            print("List is empty")
+            return
+        temp = self.head
+        while temp:
+            # Traverse the list and print each node's data
+            print(temp.data, end=" ")
+            temp = temp.next
         print()
 
+    @staticmethod
     def merge(l1, l2):
-        a = l1.head
-        b = l2.head
-        l3 = LinkedList()
-        while l1 and l2:
+        # Merge two sorted linked lists into a single sorted linked list
+        a, b = l1.head, l2.head  # Pointers to the heads of the two lists
+        l3 = LinkedList()  # Create a new linked list to store the merged result
+        tail = None  # Tail pointer for appending nodes in sorted order
+
+        while a and b:
+            # Compare the data of the current nodes in both lists
             if a.data < b.data:
-                l3.insert(l1.data)
-                l1 = l1.next
+                data = a.data  # Take the smaller data
+                a = a.next  # Move to the next node in the first list
             else:
-                l3.insert(l2.data)
-                l2 = l2.next
-        while l1:
-            l3.insert(l1.data)
-            l1 = l1.next
-        while l2:
-            l3.insert(l2.data)
-            l2 = l2.next
-        return l3
+                data = b.data  # Take the smaller data
+                b = b.next  # Move to the next node in the second list
+
+            if l3.head is None:
+                # If the merged list is empty, initialize it with the first node
+                l3.head = Node(data)
+                tail = l3.head
+            else:
+                # Otherwise, append the new node to the merged list
+                tail.next = Node(data)
+                tail = tail.next
+
+        while a:
+            # Append the remaining nodes from the first list (if any)
+            if l3.head is None:
+                l3.head = Node(a.data)
+                tail = l3.head
+            else:
+                tail.next = Node(a.data)
+                tail = tail.next
+            a = a.next
+
+        while b:
+            # Append the remaining nodes from the second list (if any)
+            if l3.head is None:
+                l3.head = Node(b.data)
+                tail = l3.head
+            else:
+                tail.next = Node(b.data)
+                tail = tail.next
+            b = b.next
+
+        return l3  # Return the merged sorted linked list
 
 
-a = LinkedList()
-b = LinkedList()
+# Read input
+a = LinkedList()  # Create the first linked list
+b = LinkedList()  # Create the second linked list
 
-
+# Populate the first linked list with user input
 for data in map(int, input().split()):
-    a.insert(data)
+    a.append(data)
 
-
+# Populate the second linked list with user input
 for data in map(int, input().split()):
-    b.insert(data)
+    b.append(data)
 
-
-LinkedList.merge(a, b).display()
+# Merge the two sorted linked lists and display the result
+merged_list = LinkedList.merge(a, b)
+merged_list.display()
